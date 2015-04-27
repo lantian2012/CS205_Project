744e278db0c15d9c35a6c8b6be11a8e4178d59b3
Yuna <danyangchen@g.harvard.edu>
2015-04-27 17:36:56 -0400

    update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -1,3 +1,8 @@
+###
+### file for pre-processing of the images.
+### run it with: python pre_process.py "Data/train001" 1024 1536 0.05
+###
+
 from skimage import exposure
 from skimage.color import rgb2gray
 from skimage import io
@@ -10,25 +15,29 @@ import sys
 
 def pre_process(directories, output_shape, clip_limit=0.03):
 	for directory in directories:
+		out_directory = directory + "_processed"		
+		if not os.path.exists(out_directory):
+			os.makedirs(out_directory)
+    				
 		for filename in os.listdir(directory):
-	 		im = io.imread(filename)
- 			im = rgb2gray(im)
- 			im = resize(im, output_shape) 
- 			im = exposure.equalize_adapthist(image, clip_limit=clip_limit)
-			out_directory = directory + "_processed"		
-			if not os.path.exists(out_directory):
-    				os.makedirs(out_directory)
-			io.imsave(out_directory + "/" + filename, im)
+			if filename.endswith(".jpeg"):
+				im = io.imread(directory + "/" + filename)
+				im = rgb2gray(im)
+				im = resize(im, output_shape) 
+				im = exposure.equalize_adapthist(im, clip_limit=clip_limit)
+				io.imsave(out_directory + "/" + filename, im)
 	return True
 	
 # read parameters
-if (len(sys.argv) == 4):
-    directories = sys.argv[1]
-    output_shape = sys.argv[2]
-    clip_limit = float(sys.argv[3])
+if (len(sys.argv) == 5):
+    directories = list(sys.argv[1].split(","))
+    output_shape_height = int(sys.argv[2])
+    output_shape_width = int(sys.argv[3])
+    clip_limit = float(sys.argv[4])
 else: 
-    sys.exit("Usage : python pre_process.py directories output_shape clip_limit \n Example: python pre_process.py \"Data/train001\" (1024,1024) 0.05")
+    sys.exit("Usage : python pre_process.py directories output_shape_height output_shape_width clip_limit \n Example: python pre_process.py \"Data/train001,Data/train002\" 1024 1536 0.05")
 
-print type(output_shape)
 # pre process the image
-#pre_process(directories, output_shape, clip_limit)
+output_shape = (output_shape_height,output_shape_width)
+pre_process(directories, output_shape, clip_limit)
+
