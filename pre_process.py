4ada00517245d53f62aabbe6e6eb96ff668d2881
cid2105 <cole.ian.diamond@gmail.com>
2015-04-27 19:02:26 -0400

    fixed some bugs. but looks like adaptive hist is giving wrong output. not sure whats wrong. Danyang please fix.

--- pre_process.py
+++ pre_process.py
@@ -14,7 +14,7 @@ import yaml
 # INPUT: list of directories, output_shape, clip_limit
 # OUTPUT: Preprocessed Images in directory_processed
 
-def pre_process(directories, output_shape, clip_limit=0.03):
+def pre_process(directories, output_shape, adaptive_histogram, clip_limit=0.03):
 	for directory in directories:
 		out_directory = directory + "_processed"		
 		if not os.path.exists(out_directory):
@@ -25,18 +25,19 @@ def pre_process(directories, output_shape, clip_limit=0.03):
 				im = io.imread(directory + "/" + filename)
 				im = rgb2gray(im)
 				im = resize(im, output_shape) 
-				im = exposure.equalize_adapthist(im, clip_limit=clip_limit)
+				if adaptive_histogram:
+					im = exposure.equalize_adapthist(im, clip_limit=clip_limit)
 				io.imsave(out_directory + "/" + filename, im)
 	return True
 
-def get_pre_process_params(config_file_name)
+def get_pre_process_params(config_file_name):
 	config = open("config/" + config_file_name)
-	config_dict = yaml.safe_read(config)
-	locals().update(config_dict['pre_process'])
-	locals().update(config_dict['pre_process']['adaptive_histogram'])
-	return directories, output_shape, adaptive_histogram, clip_limit 
+	config_dict = yaml.safe_load(config)['pre_process']
+	return config_dict['directories'], map(int, config_dict['output_shape']), config_dict['adaptive_histogram']['adaptive_histogram'], float(config_dict['adaptive_histogram']['clip_limit'])
+
 
 if __name__ == "__main__":
-	config_file_name = if len(sys.argv) > 1: sys.argv[1] else "default.yaml"
+	config_file_name = sys.argv[1] if len(sys.argv) > 1 else "default.yaml"
 	directories, output_shape, adaptive_histogram, clip_limit = get_pre_process_params(config_file_name)
+	print directories, output_shape, adaptive_histogram, clip_limit
 	pre_process(directories, output_shape, adaptive_histogram, clip_limit)
