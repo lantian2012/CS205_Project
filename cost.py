da21f3b0c25f8bb69eff1172697ed7cbcfcc94e2
cid2105 <cole.ian.diamond@gmail.com>
2015-04-28 01:35:35 -0400

    reset cost to original

--- cost.py
+++ cost.py
@@ -7,34 +7,13 @@ from pylearn2.models.model import Model
 from pylearn2.space import VectorSpace
 from pylearn2.utils import sharedX
 
-
-class LogisticRegression(Model):
-    def __init__(self, nvis, nclasses):
-        super(LogisticRegression, self).__init__()
-
-        self.nvis = nvis
-        self.nclasses = nclasses
-
-        W_value = numpy.random.uniform(size=(self.nvis, self.nclasses))
-        self.W = sharedX(W_value, 'W')
-        b_value = numpy.zeros(self.nclasses)
-        self.b = sharedX(b_value, 'b')
-        self._params = [self.W, self.b]
-
-        self.input_space = VectorSpace(dim=self.nvis)
-        self.output_space = VectorSpace(dim=self.nclasses)
-
-    def logistic_regression(self, inputs):
-        return T.nnet.softmax(T.dot(inputs, self.W) + self.b)
-
 class QuadraticKappaCost(DefaultDataSpecsMixin, Cost):
     supervised = True
 
     def expr(self, model, data, **kwargs):
         space, source = self.get_data_specs(model)
         space.validate(data)
-
         inputs, targets = data
         y_hat = model.fprop(inputs)
         loss = quadratic_kappa_cost(y_hat, targets)
-        return loss.mean()
+        return loss
