d6d0ccde2c69b852863409b744deef0d018bd200
cid2105 <cole.ian.diamond@gmail.com>
2015-05-03 18:14:24 -0400

    fixed small bug in runner.py

--- runner.py
+++ runner.py
@@ -1,12 +1,11 @@
 import h5py
 import pylearn2
 import theano
-train = open('config/conv.yaml', 'r').read()
 import sys
 import yaml
 
-conv_yaml = "config/" + sys.argv[1] if len(sys.argv) > 1 else "config/conv.yaml"
-config_yaml = "config/" + sys.argv[2] if len(sys.argv) > 2 else "config/default.yaml"
+config_yaml = "config/" + sys.argv[1] if len(sys.argv) > 1 else "config/default.yaml"
+conv_yaml = "config/" + sys.argv[2] if len(sys.argv) > 2 else "config/conv.yaml"
 hdf5_file = yaml.safe_load(open(config_yaml))['pre_process']['hdf5_file']
 
 train = open(conv_yaml, 'r').read()
@@ -32,6 +31,7 @@ print train
 print theano.config.device
 from pylearn2.config import yaml_parse
 train = yaml_parse.load(train)
+print train
 train.main_loop()
 #print_monitor.py convolutional_network_best.pkl | grep test_y_misclass
 #!show_weights.py convolutional_network_best.pkl
